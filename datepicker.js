64475fa4d4cdc63448233dc384af03ff808783a3
Boris <boris@actived.fr>
2020-07-08 11:39:59 +0200

    [+] Force light theme on iOS and remove use of deprecated methods

--- datepicker.js
+++ datepicker.js
@@ -50,9 +50,10 @@ class DatePicker extends Component {
     this.setModalVisible = this.setModalVisible.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.date !== this.props.date) {
-      this.setState({date: this.getDate(nextProps.date)});
+  // --  fix use of deprecated lifecycle method
+  componentDidUpdate(prevProps) {
+    if (prevProps.date !== this.props.date) {
+      this.setState({date: this.getDate(this.props.date)});
     }
   }
 
